23fb2f330ce296fa6447dfce221f86ff849b1664
ObaidurRehman <obaid.urrehman@gmail.com>
2017-11-18 00:33:01 +0500

    chore: step 1

--- src/smart-ui.module.ts
+++ src/smart-ui.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SmartTextMaskDirective } from './smart-text-mask/smart-text-mask.directive';
-import { SmartSelectComponent } from './smart-select/smart-select.component';
-import { OutsideClickDirective } from './smart-select/outside-click.directive';
-import { SearchPipe } from './smart-select/pipes/search.pipe';
 
 @NgModule({
     imports: [
@@ -12,15 +8,9 @@ import { SearchPipe } from './smart-select/pipes/search.pipe';
         FormsModule
     ],
     exports: [
-        SmartTextMaskDirective, 
-        SmartSelectComponent,
-        CommonModule
+        CommonModule,
     ],
     declarations: [
-        SmartTextMaskDirective, 
-        SmartSelectComponent, 
-        OutsideClickDirective, 
-        SearchPipe
     ],
     providers: [],
 })
